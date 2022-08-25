611b7e2b22de4c5378afd205e28d795c2fe27142
Chris Kuethe <chris.kuethe@gmail.com>
2022-08-24 19:58:45 -0700

    rename to lucam_ng

--- lucam/__init__.py
+++
@@ -1,4 +0,0 @@
-# lucam/__init__.py
-
-from .lucam import __doc__, __all__, __version__
-from .lucam import *
---
+++ lucam_ng/__init__.py
@@ -0,0 +1,4 @@
+# lucam/__init__.py
+
+from .lucam_ng import __doc__, __all__, __version__
+from .lucam_ng import *
